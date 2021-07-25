bfe5b7e1107635e89e5a87ab9b0f486ef2cdc914
paulinellagas <paulinellagas@gmail.com>
2021-07-25 18:56:41 +0800

    dashboard

---
+++ Modules/Dashboard/Controllers/Dashboards.php
@@ -0,0 +1,18 @@
+<?php namespace Modules\Dashboard\Controllers;
+
+use App\Controllers\BaseController;
+use Modules\Dashboard\Models as Dashboard;
+
+class Dashboards extends BaseController {
+
+  function __construct(){
+    $this->dashboardsModel = new Dashboard\DashboardsModel();
+  }
+
+  public function index(){
+
+    $data['view'] = 'Modules\Dashboard\Views\dashboards\index';
+    return view('template/index', $data);
+  }
+
+}
